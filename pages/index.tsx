eb95dd31d0fc704ea5e751ac517600ca72814cce
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-07-01 19:15:41 +0800

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,6 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
