bb8ce68b73762968982a991f1d14745d80fe5fcf
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-07-06 23:03:31 +0800

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,22 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a> on Razzo!
         </h1>
+        <div className={styles.grid}>
+        </div>
       </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
     </div>
   )
 }
