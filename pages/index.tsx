e7c71299ae109ac1f3f370f14c71e82a78995a55
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-07-09 18:37:17 +0800

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,10 +24,6 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
         </a>
       </footer>
     </div>
