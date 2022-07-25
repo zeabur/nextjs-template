df3854f63046c849a4ff13420eb95c6d62582ab4
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-25 13:13:49 +0800

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ const Home: NextPage = () => {
         <p>haha</p>
       </main>
 
+
       <footer className={styles.footer}>
           Powered by{' '}
           <span className={styles.logo}>
