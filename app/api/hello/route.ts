2bda97ba9a753aebf72b01e387417ebb2b3a7001
Yuanlin Lin <im.yuanlinlin@gmail.com>
2023-11-21 16:42:10 +0800

    change api route to get from post

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
 
-export async function POST(req: any) {
+export async function GET() {
   return NextResponse.json({message: 'This is a Next.js API route powered by Zeabur serverless functions!'})
 }
